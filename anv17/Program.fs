92ce61d5a7ebec5bd0e2950c9be4643689eb7c4c
Aswin Vivekanandan <aswinvivekanandan@gmail.com>
2020-02-16 23:50:58 +0000

    add initial program.fs and DGraphiser files

---
+++ anv17/Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
