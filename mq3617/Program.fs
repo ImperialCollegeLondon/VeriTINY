6a7a7cbf53941c909f38928f26f47d77c824e6d4
marqisd <mark.qi17@imperial.ac.uk>
2020-02-17 14:22:39 +0000

    Add mq3617 folder

---
+++ mq3617/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+open Module1
+
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    deleteThisSample()
+    Console.ReadKey()|>ignore
+    0 // return an integer exit code
+
