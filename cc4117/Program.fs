22240b521a32a01c76301cee52d8b1f42dead461
Channen Chong <channen2@gmail.com>
2020-02-17 14:09:54 +0000

    Add cc4117 to folder

---
+++ cc4117/Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+open Module1
+
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    deleteThisSample()
+    0 // return an integer exit code
