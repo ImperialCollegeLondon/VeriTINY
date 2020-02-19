79a13f7a3dc3538f1cb6d47b2c0b2f8c5262db5c
marqisd <mark.qi17@imperial.ac.uk>
2020-02-19 21:33:54 +0000

    Change to support Connectioniser and SharedTypes

--- mq3617/Program.fs
+++ mq3617/Program.fs
@@ -1,14 +1,9 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
+open Connectioniser
 
-open System
-
-open Module1
-
-
+UserIn (0)
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    deleteThisSample()
     Console.ReadKey()|>ignore
     0 // return an integer exit code
 
