adea45a54c0d92229fe900697ef347125091f63f
Channen Chong <channen2@gmail.com>
2020-02-19 20:21:28 +0000

    Create example connection list in Evaluator

--- cc4117/Evaluator.fs
+++ cc4117/Evaluator.fs
@@ -1,1 +1,49 @@
 module Evaluator
+open SharedTypes
+
+
+
+// GeneralNet = bool * NamedNet
+// NamedNet = string * Net
+// Net = | Wire of Map<int,LogicLevel> | Bus of Map<int,LogicLevel>
+
+// test purposes
+let GenNetListInA : GeneralNet list = 
+    [false, ("A0", Wire (Map [0, Low]));
+    false, ("A1", Wire (Map [0, Low]))
+    ]
+
+let GenNetListOutA : GeneralNet List =
+    [false, ("AOut", Wire (Map [0, Low]))]
+
+// test purposes
+let GenNetListInB : GeneralNet list = 
+    [false, ("B0", Wire (Map [0, Low]));
+    false, ("B1", Wire (Map [0, Low]))
+    ]
+
+let GenNetListOutB : GeneralNet list =
+    [false, ("BOut", Wire (Map [0, Low]))]
+
+let GenNetListInDFF : GeneralNet list = 
+    [false, ("BOut", Wire (Map [1, Low]))]
+
+let GenNetListOutDFF : GeneralNet list =
+    [true, ("C1", Wire (Map [0, Low]))]
+
+let GenNetListInC : GeneralNet list =
+    [false, ("C0", Wire (Map [0, Low]));
+     true, ("C1", Wire (Map [0, Low]))
+    ]
+
+let GenNetListOutC : GeneralNet list = 
+    [false, ("COut", Wire (Map [0, Low]))]
+
+/// example connection list
+let connectListEx = 
+    [("Module A", GenNetListInA, GenNetListOutA);
+    ("Module B", GenNetListInB, GenNetListOutB);
+    ("DFF", GenNetListInDFF, GenNetListOutDFF);
+    ("Module C", GenNetListInC, GenNetListOutC)
+    ]
+
