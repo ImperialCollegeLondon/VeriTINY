0e1921a0d0c8e37ec54ed654fa76a9fdc141ee55
Aswin Vivekanandan <aswinvivekanandan@gmail.com>
2020-02-20 11:27:42 +0000

    add function to convert expression to appropriate DGraphOpNode and DGraphEdges

--- anv17/DGraphiser.fs
+++ anv17/DGraphiser.fs
@@ -39,6 +39,11 @@ type NetNameToken = |Name of string|OpenSqBracket|SliceIndex of int|SemiColon|Cl
 let getNetIDFromStr (netIdentifierStr: string) : NetIdentifier = failwithf "Not implemented yet - Requires Lexer Module"
 
 
+let getNetNodeByName name netNodeLst =
+    match List.tryFind (fun node -> node.NetName = name) netNodeLst with
+    |Some node -> node
+    |None -> failwithf "Cannot find DGraphNetNode with name %s" name   
+
 let formNetIDsFromStrLst netIDStrLst =
     let foldNetIDStr netIDs netIDStr =
         [getNetIDFromStr netIDStr]
@@ -64,3 +69,37 @@ let formNetNodesFromIDs netIDLst =
   
 
     List.fold foldNetIDtoNode [] netIDLst
+
+
+let formEdgesAndOpNodes netNodeLst exprLst = 
+
+    let processExpression (op, inputs, outputs) =
+        let opNode = op
+
+        let exprInputNetIDS = formNetIDsFromStrLst inputs
+        let exprOutputsNetIDS = formNetIDsFromStrLst outputs 
+
+        let formInputEdge opNode netID = 
+            {
+                Input = getNetNodeByName netID.Name netNodeLst
+                Output = opNode
+                SliceIndices = netID.SliceIndices
+                IsSliceOfInput = true
+            }
+
+        let formOutputEdge opNode netID = 
+            {
+                Input = opNode
+                Output = getNetNodeByName netID.Name netNodeLst
+                SliceIndices = netID.SliceIndices
+                IsSliceOfInput = false
+            }
+
+        let edgeFolder idToEdge edgLst netID = List.append edgLst (idToEdge opNode netID)
+
+        let inputEdges = ([], exprInputNetIDS) ||> List.fold (edgeFolder formInputEdge)
+        let outputEdges = ([], exprOutputsNetIDS) ||> List.fold (edgeFolder formOutputEdge)
+
+        List.append inputEdges outputEdges
+
+        
