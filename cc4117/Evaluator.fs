7b3d993d45e4f47e4effce0604017ca1de7ec191
Channen Chong <channen2@gmail.com>
2020-02-20 14:38:41 +0000

    Add methods to extract Nets and Logic Levels

--- cc4117/Evaluator.fs
+++ cc4117/Evaluator.fs
@@ -6,7 +6,6 @@ open SharedTypes
 // NamedNet = string * Net
 // Net = | Wire of Map<int,LogicLevel> | Bus of Map<int,LogicLevel>
 
-let hehe = 2
 // test purposes
 let GenNetListInA : GeneralNet list = 
     [false, ("A0", Wire (Map [0, Low]));
@@ -49,7 +48,62 @@ let connectListEx : Connection list=
     (Name "Module C", GenNetListInC, GenNetListOutC)
     ]
 
-// given Connection list, find all synchronous elements
-let updateDFF (input:Connection) = 
-    let Name block, genNetIn, genNetOut = input
-    if block = "DFF" then sprintf "hi" else sprintf "no"
+// to update a genNet need to know:
+// if bus: which wire in a bus (0, 1, 2) 
+// if wire: just change the wire (0) to new logicLevel
+// can't update a map, so need to rewrite the whole map...
+// GenNet type example: false, ("BOut", Wire (Map [1, Low]))
+let BusEx : GeneralNet =
+    false, ("BusA", Bus(Map [0, Low; 1, High; 2, Low; 3, High]))
+let wireEx : GeneralNet =
+    false, ("WireA", Wire(Map [0, Low]))
+
+
+/// extract Net from GeneralNet
+let extractNet (genNetIn: GeneralNet): Net =
+    match genNetIn with
+    | (_, (_, Wire netMap)) -> 
+        Wire netMap
+    | (_, (_, Bus netMap)) ->
+        Bus netMap
+    
+
+/// extract LogicLevel from Net
+let extractLogLevel (netIn: Net) =
+    match netIn with
+    | Wire netMap
+    | Bus netMap ->
+    netMap |> Map.toList |> List.map snd
+     
+
+let netUpdate (genNetIn: GeneralNet) (index: int) (newLog:LogicLevel) : GeneralNet =
+    match genNetIn with
+    | (sync, (str, Bus netMap)) ->
+        sync, (str, Bus(Map.add index newLog netMap))
+    | (sync, (str, Wire netMap)) -> 
+        sync, (str, Wire(Map.add 0 newLog netMap))
+
+//given Connection list, find all synchronous elements
+// let updateDFF (input:Connection) : Connection = 
+//     let Name block, genNetIn, genNetOut = input
+//     let sync, (str, Bus )
+//     if block = "DFF" then
+//     let indexList = m |> Map.toList |> List.map fst
+//     else
+//     input
+
+    
+// let genNetWireUpdate (inp: GeneralNet) newLog : GeneralNet = 
+//     match inp with
+//     | false, (str, net) ->
+//         false, (str, wireUpdate newLog)
+//     | true, (str, net) -> 
+//         true, (str, wireUpdate newLog)
+
+
+// how to update maps 
+let ym = Map ["cat",7 ; "the",3]
+let ym' = Map.add "cat" 5 ym
+
+    
+
