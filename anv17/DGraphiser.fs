5c58a65419f58d057a3fd05d0ee7a7fe03362bbd
Aswin Vivekanandan <aswinvivekanandan@gmail.com>
2020-02-20 23:55:36 +0000

    add functions for decimal/binary conversion using LogicLevel lists

--- anv17/DGraphiser.fs
+++ anv17/DGraphiser.fs
@@ -138,10 +138,10 @@ let createNamedNetList netNodes =
     List.fold foldNetNodesToNamedNets [] netNodes
 
 
-let updateBus bus (a, b) newLogicLevels = 
+let updateBus bus (a,b) newLogicLevels = 
  
     if (b - a + 1) <> (List.length newLogicLevels) || not (Map.containsKey b bus) || not (Map.containsKey a bus)
-    then failwith "Cannot update bus, given bus slice is not the same size as logic level list"
+    then failwith "Cannot update bus, error in given bus slice"
     else
         bus
         |> Map.map (fun key oldVal ->
@@ -149,3 +149,21 @@ let updateBus bus (a, b) newLogicLevels =
             then newLogicLevels.[key - a]
             else oldVal
             )
+
+let updateWire wire newLogicLevel = Map.add 0 newLogicLevel wire
+
+let rec uintToLogicLevelList uint logicLvlLst =
+    if uint = 0
+    then logicLvlLst
+    else
+        if (uint % 2) = 1
+        then uintToLogicLevelList (uint / 2) (logicLvlLst @ [High])
+        else uintToLogicLevelList (uint / 2) (logicLvlLst @ [Low])
+
+let logicLevelsToUint logicLvlLst = 
+    let getDecimalValue bitPos bit = 
+        if bit = High
+        then int ((float 2) ** (float bitPos))
+        else 0
+    List.mapi getDecimalValue logicLvlLst
+    |> List.reduce (+)
--- anv17/Program.fs
+++ anv17/Program.fs
@@ -1,10 +1,14 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open DGraphiser
 
 
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
+    let decNum = 5
+    let binNum = uintToLogicLevelList decNum []
+    printfn "%A" binNum
     Console.ReadKey() |> ignore
     0 // return an integer exit code
