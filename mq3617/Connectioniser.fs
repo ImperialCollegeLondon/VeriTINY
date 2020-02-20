3c25c164d0eeb82f60611d73b6be8d181e6f8820
marqisd <mark.qi17@imperial.ac.uk>
2020-02-20 13:27:32 +0000

    Added unique net definitions

--- mq3617/Connectioniser.fs
+++ mq3617/Connectioniser.fs
@@ -10,22 +10,38 @@ let  l1 ={
     Wires= ["E";"F"]
     ExpressionList = [(And,["A";"B"],["E"]);(And,["C";"D"],["F"]);(And,["E";"F"],["G"])]
 }
-let avaliableBlocks = [l1]
+let  l2 ={  
+    Name = "Test2"
+    Inputs=["A";"B";"C";"D"]
+    Outputs = ["G"]
+    Wires= ["E";"F"]
+    ExpressionList = [(And,["A";"B"],["E"]);(And,["C";"D"],["F"]);(And,["E";"F"],["G"])]
+}
+let avaliableBlocks = [l1;l2]
 //////////////////////////////////////////////////////////////////////
+let first (a, _, _) = a
+let second (_, b, _) = b
+let third (_, _, c) = c
+
 let MakeConnection (net1:GeneralNet)(net2:GeneralNet) =
     ((fst net1 || fst net2) , snd net1)
 
 let rec genGenNets (blist:string list)=
-    List.map (fun str ->[false, (str, Wire (Map [0, Low]))]) blist  //only works for unclocked wires
+    List.map (fun str ->false, (str, Wire (Map [0, Low]))) blist  //only works for unclocked wires
 
 let searchBlocks name (block:TLogic)=
     match block.Name with
     |n when n =name-> true
     |_->false
 
+let searchConnections name (conn:Connection)=
+    match conn with
+    |n when n = name-> true
+    |_->false
+
 let genConnections name blist=
     let mBlock = (List.filter (searchBlocks name) blist).Head
-    (name,genGenNets mBlock.Inputs,genGenNets mBlock.Outputs)
+    (Name name,genGenNets mBlock.Inputs,genGenNets mBlock.Outputs)
 
 let rec AddMegaBlock []=
     match Console.ReadLine() with
@@ -34,9 +50,15 @@ let rec AddMegaBlock []=
     |str -> printf "NANI?! match failed when adding megablocks, no block exists with name %s" str
             AddMegaBlock []
 
-
+let rec refactor (blist: Connection list) =
+    printf "ref starts"
+    match blist.Head with
+    |connection when (blist.Tail).Length=0 -> [connection]
+    |connection -> ((first connection),(List.map (fun (x:GeneralNet) -> (fst x),(((fst (snd x))+((blist.Length).ToString())),(snd (snd x)))) (second connection)),(List.map (fun (x:GeneralNet) -> (fst x),(((fst (snd x))+((blist.Length).ToString())),(snd (snd x)))) (third connection)))::(refactor blist.Tail)
+    |_->[]                                                                   
 let rec UserIn =
     let blockLst = AddMegaBlock []
-    printf "%A" blockLst
+    let conLst = refactor (List.sort blockLst)
+    printf "%A" conLst
     
 
