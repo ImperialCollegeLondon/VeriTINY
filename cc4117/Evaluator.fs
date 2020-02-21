1729dfe1350fe5dc85052c7720b7cdfed53a332c
Channen Chong <channen2@gmail.com>
2020-02-21 19:21:49 +0000

    Reorder functions in Evaluator

--- cc4117/Evaluator.fs
+++ cc4117/Evaluator.fs
@@ -1,11 +1,7 @@
 module Evaluator
 open SharedTypes
 open ExampleTypes
-
-
-// GeneralNet = bool * NamedNet
-// NamedNet = string * Net
-// Net = | Wire of Map<int,LogicLevel> | Bus of Map<int,LogicLevel>
+open Helper
 
 let extractNet (genNetIn: GeneralNet): Net =
     match genNetIn with
@@ -20,29 +16,32 @@ let extractLogLevel (netIn: Net) =
     | Bus netMap ->
     netMap |> Map.toList |> List.map snd
 
+let extractGenNetLstIn (cIn: Connection) =
+    let _, genNetLstIn, _ = cIn
+    genNetLstIn
+  
+let extractGenNetLstOut (cIn: Connection) =
+    let _, _, genNetLstOut = cIn
+    genNetLstOut 
+
 let netSize (netIn: Net): int =
     match netIn with
     | Wire netMap
     | Bus netMap ->
     netMap |> Map.toList |> List.map fst |> List.length
 
-/// find length of output nets from genNetOut list 
-let findLength (lst: GeneralNet list) =
+let findLengths (lst: GeneralNet list) =
     List.map (extractNet >> netSize) lst
 
-// tail recursion
 // generate list of lists of LogicLevel of appropriate size for output
-let rec splitLst acc (logLst:LogicLevel list) (lstOfLengths: int list) =
+let rec groupLogic acc (logLst:LogicLevel list) (lstOfLengths: int list) =
     match lstOfLengths with
     | hd::tl ->
         let acc', logLst' = List.splitAt hd logLst
-        splitLst (acc @ [acc']) logLst' tl
+        groupLogic (acc @ [acc']) logLst' tl
     | [] -> 
         acc
 
-let splitTest = splitLst [] [High;Low;High;Low;High;Low;High;Low] [1;3;3;1]
-
-// rebuild GeneralNet with newMap
 let updateNet (genNetIn: GeneralNet) newMap =
     match genNetIn with
     | (sync, (str, Wire _)) -> 
@@ -50,37 +49,35 @@ let updateNet (genNetIn: GeneralNet) newMap =
     | (sync, (str, Bus _)) ->
         sync, (str, Bus newMap)
 
-/// operate on two lists 
-let rec lstOpParallel acc f lstA lstB =
-        match lstA, lstB with
-        | h1::t1, h2::t2 ->
-            let acc' = f h1 h2
-            lstOpParallel (acc @ [acc']) f t1 t2
-        | _ -> 
-            acc
+let updateGenLst genLst newMaps =
+    lstOpParallel [] updateNet genLst newMaps    
 
-// will need to take megablock, function to evaluate outputs
-// also need genNetLstIn to evaluate, and genNetLstOut to know what map to create
-// currently just takes input and passes to output (DFF functionality)
-let generateNewMap genNetLstIn genNetLstOut =
-    let newLog = List.collect (extractNet >> extractLogLevel) genNetLstIn 
-    let lstOfLengths = findLength genNetLstOut
+let takePrevInputs genNetLstIn =
+    List.collect (extractNet >> extractLogLevel) genNetLstIn 
 
-    let grpLog = splitLst [] newLog lstOfLengths
+let generateNewMapLst f genNetLstIn genNetLstOut =
+    let newLogic = f genNetLstIn 
+    let lstOfLengths = findLengths genNetLstOut
+
+    let grpsOfLogic = groupLogic [] newLogic lstOfLengths
     let generateList n = [0..n-1]
-    let grpNum = List.map generateList lstOfLengths
+    let grpsOfNum = List.map generateList lstOfLengths
 
-    let zipOut = lstOpParallel [] List.zip grpNum grpLog
-    zipOut |> List.map Map.ofList
+    (grpsOfNum,grpsOfLogic) ||> lstOpParallel [] List.zip 
+    |> List.map Map.ofList
 
 let updateDFF genNetLstIn genNetLstOut =
-    // generateNewMap will have an input argument that takes in "DFF"
-    let newMapLst = generateNewMap genNetLstIn genNetLstOut
-    lstOpParallel [] updateNet genNetLstOut newMapLst
+    let newMapLst = generateNewMapLst takePrevInputs genNetLstIn genNetLstOut
+    (genNetLstOut, newMapLst) ||> lstOpParallel [] updateNet 
 
-let updateDFFTest = updateDFF dffMixedIn dffMixedOut
 
-//given a genNetlist, check if synchronous or not
+let setToLow (genNetLst:GeneralNet list) =
+    let lstOfLengths = findLengths genNetLst
+    let newMaps = List.map createNewMap lstOfLengths
+    updateGenLst genNetLst newMaps
+    
+
+//given a genNet, check if synchronous or not
 let syncCheck (genNet:GeneralNet) = 
     match genNet with
     | false, _ ->
@@ -88,14 +85,28 @@ let syncCheck (genNet:GeneralNet) =
     | true, _ ->
         true
 
-let filterNotSync (genNetLst:GeneralNet list) =
+// given a list of GenNets, filter out the GenNets that are asynchronous
+let removeNotSync (genNetLst:GeneralNet list) =
     List.filter syncCheck genNetLst
 
+
+
+let filterSync (cInLst:Connection list) =
+    List.map (extractGenNetLstIn >> removeNotSync) cInLst
+        
+// genNet
+// |> extractNet
+// |> netSize
+// |> createNewMap
 /// initialize synchronous nets (outputs of DFFs) to 0 
-let initializeSync (cIn:Connection) =
+let initializeSync (connectionLst:Connection List) =
+    connectionLst |> List.map extractGenNetLstIn 
+    // need to use updateNet, which requires newMap ... 
+
+    
+
     // search for all synchronous nets then update them to 0
-    let _, genLstIn, genLstOut = cIn
-    filterNotSync genLstOut
+    
     // then set all of the nets to 0 using logicLst of "LOW" length N 
     
 // how to use memoisation:
@@ -113,13 +124,3 @@ let initializeSync (cIn:Connection) =
 // cLst = connection List 
 // megaLst = megablock list    type megablock = Name of String   don't need this?
 // let advanceState netLst cLst megaLst =
-
-
-//given Connection list, find all synchronous elements
-// let updateDFF (input:Connection) : Connection = 
-//     let Name block, genNetIn, genNetOut = input
-//     let sync, (str, Bus )
-//     if block = "DFF" then
-//     let indexList = m |> Map.toList |> List.map fst
-//     else
-//     input
