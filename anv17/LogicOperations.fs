faa3dcc4cf65d49319882aad8947a7e9be5cd85a
Aswin Vivekanandan <aswinvivekanandan@gmail.com>
2020-02-23 12:20:55 +0000

    add LogicOperations Module

--- anv17/LogicOperations.fs
+++ anv17/LogicOperations.fs
@@ -1,1 +1,38 @@
 module LogicOperations
+open SharedTypes
+
+let ANDOp logicLvl1 logicLvl2 = 
+    if logicLvl1 = High then logicLvl2 else Low
+
+let OROp logicLvl1 logicLvl2 =
+    if logicLvl1 = High then High else logicLvl2
+
+let NOTOp logicLvl = 
+    if logicLvl = High then Low else High
+
+let extractLogicLevel logicLvlOpt = 
+     match logicLvlOpt with
+        |Some logicLvl -> logicLvl
+        |None -> failwith "Logic Level has not been assigned yet"
+
+
+let apply2OpToBus op (bus1: Map<int,LogicLevel option>, bus1StartIndex: int) (bus2: Map<int,LogicLevel option>, bus2StartIndex : int) = 
+    let getCorrespondingNet2Index net1Index = bus2StartIndex + net1Index - bus1StartIndex
+    Map.toList bus1
+    |> List.mapi (fun i (mapIndex, logicLvlOpt) ->
+        let logicLvl1 = extractLogicLevel logicLvlOpt
+        let logicLvl2 = extractLogicLevel bus2.[getCorrespondingNet2Index mapIndex]
+        i, Some (op logicLvl1 logicLvl2))
+    |> Map
+
+let ANDOpBus (bus1: Map<int,LogicLevel option>, bus1StartIndex: int) (bus2: Map<int,LogicLevel option>, bus2StartIndex : int) =
+    apply2OpToBus ANDOp (bus1, bus1StartIndex) (bus2, bus2StartIndex)
+
+let OROpBus (bus1: Map<int,LogicLevel option>, bus1StartIndex: int) (bus2: Map<int,LogicLevel option>, bus2StartIndex : int) =
+    apply2OpToBus OROp (bus1, bus1StartIndex) (bus2, bus2StartIndex)
+
+let NOTOpBus (bus: Map<int, LogicLevel option>) =
+    Map.map (fun _ logicLevelOpt -> 
+        extractLogicLevel logicLevelOpt
+        |> NOTOp
+        |> Some) bus
