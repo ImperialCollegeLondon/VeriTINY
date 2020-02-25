8b5f43f40d91d53ba2cbb90953a7cb60810850e0
Aswin Vivekanandan <aswinvivekanandan@gmail.com>
2020-02-25 13:41:26 +0000

    add test code for formEvalNets

--- anv17/Program.fs
+++ anv17/Program.fs
@@ -4,9 +4,56 @@ open System
 open SharedTypes
 open CombEval
 
+let testTLogic = 
+    { Name = "mux2_gate23"
+      ExpressionList =
+                      [(Not, [{ Name = "selb"
+                                SliceIndices = None }], [{ Name = "sel"
+                                                           SliceIndices = None }]);
+                       (And, [{ Name = "out1"
+                                SliceIndices = None }],
+                        [{ Name = "a"
+                           SliceIndices = None }; { Name = "sel"
+                                                    SliceIndices = None }]);
+                       (And, [{ Name = "out2"
+                                SliceIndices = None }],
+                        [{ Name = "b"
+                           SliceIndices = None }; { Name = "selb"
+                                                    SliceIndices = None }]);
+                       (Or, [{ Name = "out"
+                               SliceIndices = Some (0, None) }],
+                        [{ Name = "out1"
+                           SliceIndices = None }; { Name = "out2"
+                                                    SliceIndices = None }]);
+                       (Not, [{ Name = "out"
+                                SliceIndices = Some (1, None) }],
+                        [{ Name = "out"
+                           SliceIndices = Some (0, None) }]);
+                       (And, [{ Name = "out"
+                                SliceIndices = Some (2, Some 0) }],
+                        [{ Name = "out"
+                           SliceIndices = Some (2, Some 0) }])]
+      Inputs =
+              [{ Name = "a"
+                 SliceIndices = None }; { Name = "b"
+                                          SliceIndices = None };
+               { Name = "sel"
+                 SliceIndices = None }]
+      Outputs = [{ Name = "out"
+                   SliceIndices = Some (2, Some 0) }]
+      Wires =
+             [{ Name = "out1"
+                SliceIndices = None }; { Name = "out2"
+                                         SliceIndices = None };
+              { Name = "selb"
+                SliceIndices = None }] }
+
+let 
 
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
+    let evalNets = formEvalNets testTLogic
+    printfn "%A" evalNets
     Console.ReadKey() |> ignore
     0 // return an integer exit code
