0a7bff0e6f0897c4d41160f8fdf4d0fd792e94d4
Channen Chong <channen2@gmail.com>
2020-02-27 14:22:46 +0000

    Reorganize code in Evaluator module

--- cc4117/Evaluator.fs
+++ cc4117/Evaluator.fs
@@ -3,58 +3,50 @@ open SharedTypes
 open ExampleTypes
 open Helper
 
+
 let extractGenNetLsts (cIn: Connection) =
     let _, lstIn, lstOut = cIn
     lstIn, lstOut
 
-let extractNetName (gNet: GeneralNet) =
-    match gNet with
-    | (_, (str, _)) -> str
-
 let extractNet (gNet: GeneralNet): Net =
-    match gNet with
-    | (_, (_, Wire netMap)) -> 
-        Wire netMap
-    | (_, (_, Bus netMap)) ->
-        Bus netMap
-    
-let extractLogLevel (netIn: Net) =
-    match netIn with
-    | Wire netMap
-    | Bus netMap ->
-    netMap |> Map.toList |> List.map snd
+        match gNet with
+        | (_, (_, Wire netMap)) -> 
+            Wire netMap
+        | (_, (_, Bus netMap)) ->
+            Bus netMap
 
 let netSize (netIn: Net): int =
-    match netIn with
-    | Wire netMap
-    | Bus netMap ->
-    netMap |> Map.toList |> List.map fst |> List.length
-
-let findLengths (lst: GeneralNet list) =
-    List.map (extractNet >> netSize) lst
-
-// generate list of lists of LogicLevel of appropriate size for output
-let rec groupLogic acc lstOfLog lstOfLengths =
-    match lstOfLengths with
-    | hd::tl ->
-        let acc', lstOfLog' = List.splitAt hd lstOfLog
-        groupLogic (acc @ [acc']) lstOfLog' tl
-    | [] -> 
-        acc
+        match netIn with
+        | Wire netMap
+        | Bus netMap ->
+        netMap |> Map.toList |> List.map fst |> List.length
 
 let updateGenNet (gNet: GeneralNet) newMap =
-    match gNet with
-    | (sync, (str, Wire _)) -> 
-        sync, (str, Wire newMap) 
-    | (sync, (str, Bus _)) -> 
-        sync, (str, Bus newMap) 
+        match gNet with
+        | (sync, (str, Wire _)) -> 
+            sync, (str, Wire newMap) 
+        | (sync, (str, Bus _)) -> 
+            sync, (str, Bus newMap)
 
-let updateGenLst gNetLst newMaps =
-    lstOpParallel [] updateGenNet gNetLst newMaps    
+let extractLogLevel (netIn: Net) =
+        match netIn with
+        | Wire netMap
+        | Bus netMap ->
+        netMap |> Map.toList |> List.map snd
 
 let updateDFF gNetLstIn gNetLstOut =
+
+    // generate list of lists of LogicLevel of appropriate size for output
+    let rec groupLogic acc lstOfLog lstOfLengths =
+        match lstOfLengths with
+        | hd::tl ->
+            let acc', lstOfLog' = List.splitAt hd lstOfLog
+            groupLogic (acc @ [acc']) lstOfLog' tl
+        | [] -> 
+            acc
+
     let newLogic = List.collect (extractNet >> extractLogLevel) gNetLstIn 
-    let lstOfLengths = findLengths gNetLstOut
+    let lstOfLengths = List.map (extractNet >> netSize) gNetLstOut
 
     let grpsOfLogic = groupLogic [] newLogic lstOfLengths
     let grpsOfNum = List.map generateList lstOfLengths
@@ -67,3 +59,13 @@ let updateDFF gNetLstIn gNetLstOut =
 // let evaluateModuleWithInputs tLog inMap : Map<NetIdentifier, Net> =
 //     evaluateOutEx
   
+// let updateGenLst gNetLst newMaps =
+//     lstOpParallel [] updateGenNet gNetLst newMaps
+
+//let extractGenNetLsts (cIn: Connection) =
+//     let _, lstIn, lstOut = cIn
+//     lstIn, lstOut
+
+// let extractNetName (gNet: GeneralNet) =
+//     match gNet with
+//     | (_, (str, _)) -> str
