1745fd1f2301ccdf142c9d5b4cb1fde8d3a56b88
Louys Hong <louyshong1998@gmail.com>
2020-02-27 21:59:16 +0000

    minor edits for submission

--- tkh2017/Program.fs
+++ tkh2017/Program.fs
@@ -45,19 +45,19 @@ let parserTest3 =
 let logicBlockGeTest1 = 
     testCase "Logic Block Gen test 1" <| fun () -> 
         let expected = logicBlockGenTest1Ans
-        Expect.equal (match parserTest1Ans with | Ok ast -> convertAST ast) expected "Logic Block Gen test 1"   
+        Expect.equal (match parserTest1Ans with | Ok ast -> convertAST ast | _ -> failwithf "What?") expected "Logic Block Gen test 1"   
  
 [<Tests>]
 let logicBlockGeTest2 = 
     testCase "Logic Block Gen test 2" <| fun () -> 
         let expected = logicBlockGenTest2Ans
-        Expect.equal (match parserTest2Ans with | Ok ast -> convertAST ast) expected "Logic Block Gen test 2" 
+        Expect.equal (match parserTest2Ans with | Ok ast -> convertAST ast | _ -> failwithf "What?") expected "Logic Block Gen test 2" 
 
 [<Tests>]
 let logicBlockGeTest3 = 
     testCase "Logic Block Gen test 3" <| fun () -> 
         let expected = logicBlockGenTest3Ans
-        Expect.equal (match parserTest3Ans with | Ok ast -> convertAST ast) expected "Logic Block Gen test 3" 
+        Expect.equal (match parserTest3Ans with | Ok ast -> convertAST ast | _ -> failwithf "What?") expected "Logic Block Gen test 3" 
 
 let allTestsWithExpecto() =
         runTestsInAssembly defaultConfig [||]
