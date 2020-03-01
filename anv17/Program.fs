7dccd4c6f50d1e16e656138654b5c80adc789e38
Aswin Vivekanandan <aswinvivekanandan@gmail.com>
2020-03-01 00:17:58 +0000

    cleanup program.fs

--- anv17/Program.fs
+++ anv17/Program.fs
@@ -1,66 +1,11 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
-open SharedTypes
-open CombEval
-open EvalNetHelper
 open Tests
 open Expecto
 
-
-// let testTLogic = { 
-//    Name = "bus_and"
-//    ExpressionList =
-//                   [(And, [{ Name = "c"
-//                             SliceIndices = None }],
-//                     [{ Name = "a"
-//                        SliceIndices = Some (3, Some 1) };
-//                      { Name = "b"
-//                        SliceIndices = Some (3, Some 1) }]);
-//                    (And, [{ Name = "d"
-//                             SliceIndices = None }],
-//                     [{ Name = "a"
-//                        SliceIndices = Some (0, None) };
-//                      { Name = "b"
-//                        SliceIndices = Some (0, None) }])]
-//    Inputs =
-//           [{ Name = "a"
-//              SliceIndices = Some (3, Some 0) };
-//            { Name = "b"
-//              SliceIndices = Some (3, Some 0) }]
-//    Outputs = [{ Name = "c";SliceIndices = Some (2, Some 0) }; { Name = "d"; SliceIndices = None }]
-//    Wires = [] 
-  
-//   }
-
-// let busFromInt num busLength =
-//    intToLogicLevelList num [] 
-//    |> padLogicLvlListToLength busLength
-//    |> List.mapi (fun i el -> (i, el))
-//    |> Map
-//    |> Bus
-
-// let testInputs = Map [
-//    ({ Name = "a"; SliceIndices = Some (3, Some 0) }, busFromInt 3 4);
-//    ({ Name = "b"; SliceIndices = Some (3, Some 0) }, busFromInt 2 4)
-// ]
-
-// let canEvalExpression exprInputs evaluatedNets = 
-//             ((true), exprInputs) ||> List.fold (fun expressionEvaluatable inp ->
-//             if not expressionEvaluatable
-//             then printfn "state: %A, Expression Inputs: %A, Evaluated Nets: %A" expressionEvaluatable exprInputs evaluatedNets |> fun _ -> false
-//             else printfn "state: %A, Expression Inputs: %A, Evaluated Nets: %A, List contains: %A" expressionEvaluatable exprInputs evaluatedNets (List.contains inp evaluatedNets) |> fun _ -> (List.contains inp evaluatedNets) )
-
-// let evaluatableExpressions evaluatedNets exprToEvaluate = List.filter (fun (_, _, inpLst) -> canEvalExpression inpLst evaluatedNets) exprToEvaluate
-
 [<EntryPoint>]
 let main argv =
-   //  let evalNets = formEvalNets testTLogic
-   //  let evalNetsInputAssigned = assignInputValues testInputs evalNets
-   // //  let test = evaluatableExpressions testTLogic.Inputs testTLogic.ExpressionList
-   //  let evalNetsExprsEvaled = evaluateExprLst testTLogic.ExpressionList testTLogic.Inputs evalNetsInputAssigned
-   //  let outputNets = evaluateModuleWithInputs testTLogic testInputs
-   //  printfn "%A" outputNets
    printfn "Hello"
    runTests defaultConfig evalExprTestLst |> ignore
    runTests defaultConfig concatenationTests |> ignore
