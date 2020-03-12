dd89f2a9883af38dfbc5f95214a91c67cc26752c
Aswin Vivekanandan <aswinvivekanandan@gmail.com>
2020-03-12 21:02:51 +0000

    delete redundant files from team folder

--- team/EvalTypes.fs
+++
@@ -1,4 +0,0 @@
-module EvalTypes
-open SharedTypes
-
-type EvalNet = | EvalWire of Map<int,LogicLevel option> | EvalBus of Map<int,LogicLevel option>
--- team/SimulationTypes.fs
+++
@@ -1,5 +0,0 @@
-module SimulationTypes
-open SharedTypes
-
-type Block = Megablock * Map<NetIdentifier,Net> * Map<NetIdentifier,Net>
-
